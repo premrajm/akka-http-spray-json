54a8aaf360e22ed7c3c06c1b65a00cc9a213da27
Premraj Motling <premraj.motling@gmail.com>
2018-03-28 23:13:53 +0530

    Initial commit

---
+++ src/main/scala/com/github/premrajm/config/JsonSupport.scala
@@ -0,0 +1,20 @@
+package com.github.premrajm.config
+
+import com.github.premrajm.service.UserRegistryActor.ActionPerformed
+import com.github.premrajm.service.Users
+import com.github.premrajm.service.{ User, Users }
+
+//#json-support
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+
+trait JsonSupport extends SprayJsonSupport {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
+//#json-support
